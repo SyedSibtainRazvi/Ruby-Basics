a7376a1903ca67605a641c28215837754347d7fc
SyedSibtainRazvi <syedsibtain191@gmail.com>
2024-04-25 13:53:16 +0530

    initial commit

---
+++ studio_game.rb
@@ -0,0 +1,22 @@
+comment = "Coding is fun"
+comment1 = "Hello World"
+
+puts comment
+
+3.times do
+puts comment1
+end
+
+message = "Welcome!"
+
+3.times do
+    puts message.upcase
+    end
+
+puts Time.now
+
+puts merits = 10/2
+puts demerits = 10/2.5
+
+puts merits.class
+puts demerits.class
