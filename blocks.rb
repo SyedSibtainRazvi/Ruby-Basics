1439c11c7c4c2a589d3273c9fb958e03fd855cba
SyedSibtainRazvi <syedsibtain191@gmail.com>
2024-04-28 21:49:52 +0530

    chore: explore blocks and loops

---
+++ blocks.rb
@@ -0,0 +1,11 @@
+# we can add blocks to the methods in the class to make them more flexible.
+
+3.times {puts "Hello Sibtain"}
+
+# we can also do 
+# we can also add block parameters to the block and are available within the block
+
+10.times do  | number |
+    puts number
+    puts "Hello Sibtain"
+end
--- calculator.rb
+++ calculator.rb
@@ -11,10 +11,16 @@ class Calculator
     def subtract 
         @num1 - @num2
     end
+
+    def roll_die
+        rand(1..6)
+    end
 end
 
 
 calculation = Calculator.new(20, 10)
 
 puts calculation.add
+puts number = calculation.roll_die
 puts calculation.subtract
+
